98c6c327c37341149772b48f82bc56eae227d711
Leonard <leonard@stratitude.co.za>
2026-03-17 00:01:57 +0200

    updates

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EventEase_booking_system.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
